f68705c01d4ba2149b75fbd72c1d22a9490ec092
Đinh Hoàng Phúc <115238832+DinnBlack@users.noreply.github.com>
2025-04-25 02:24:25 +0700

    Delete dist directory

--- dist/modules/auth/dto/update-auth.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateAuthDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const login_1 = require("./login");
-class UpdateAuthDto extends (0, mapped_types_1.PartialType)(login_1.CreateAuthDto) {
-}
-exports.UpdateAuthDto = UpdateAuthDto;
-//# sourceMappingURL=update-auth.dto.js.map
--- dist/modules/users/users.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
-export declare class UsersService {
-    private readonly userRepository;
-    constructor(userRepository: Repository<User>);
-    create(email: string, password: string, username: string): Promise<User>;
-    findByEmail(email: string): Promise<User | null>;
-    findById(id: string): Promise<User | null>;
-}
