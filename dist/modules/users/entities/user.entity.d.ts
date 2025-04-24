8529e52ce8d07ab01ea25e29e4607003485da487
Phuc Dinn <phucdinn1803@gmail.com>
2025-04-25 02:18:14 +0700

    create: module auth and user

---
+++ dist/modules/users/entities/user.entity.d.ts
@@ -0,0 +1,11 @@
+export declare class User {
+    id: string;
+    email: string;
+    password: string;
+    username: string;
+    dateOfBirth: string | null;
+    isActive: boolean;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
